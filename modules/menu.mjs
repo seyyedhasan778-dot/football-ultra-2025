21daaa2577d73e1c739a4a54f79af666e129526b
seyyedhasan778-dot <seyyedhasan778@gmail.com>
2025-11-06 15:41:16 +0330

    Create menu.mjs

---
+++ modules/menu.mjs
@@ -0,0 +1,11 @@
+export function showMenu(){
+  const canvas=document.getElementById('cv'),ctx=canvas.getContext('2d');
+  canvas.width=innerWidth;canvas.height=innerHeight-80;
+  function draw(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle='#090';ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle='#fff';ctx.font='48px Tahoma';ctx.textAlign='center';
+    ctx.fillText('انتخاب تیم',canvas.width/2,100);
+  }
+  draw();
+}
